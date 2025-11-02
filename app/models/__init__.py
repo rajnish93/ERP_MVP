e627d7e178b9603df9a56b4819c8747578f4650b
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2025-11-02 17:47:11 +0530

    added employees]

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User, UserRole
 from app.models.company import Company, PlanType
+from app.models.employee import Employee
 
-__all__ = ["User", "UserRole", "Company", "PlanType"]
+__all__ = ["User", "UserRole", "Company", "PlanType", "Employee"]
 
